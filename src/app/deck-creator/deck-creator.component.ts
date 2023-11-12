4d6003da7225c24483272caef93705328eaacddf
xShyQx <bartosz.kar@wp.pl>
2023-11-12 14:55:38 +0100

    Added FontAwesome icons for DeckCreatorComponent

--- src/app/deck-creator/deck-creator.component.ts
+++ src/app/deck-creator/deck-creator.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-deck-creator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './deck-creator.component.html',
   styleUrl: './deck-creator.component.css'
 })
 export class DeckCreatorComponent {
+  editIcon = faPen
+  deleteIcon = faTrash
 }

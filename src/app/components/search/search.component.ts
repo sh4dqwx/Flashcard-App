46e4d3302c244aadb97cd10570efc7523c2cb6e1
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-11-20 21:07:45 +0100

    Added search functionality and changed online toggle

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, ElementRef, Injector, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition, faCloud, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
@@ -29,7 +29,12 @@ export class SearchComponent implements OnInit {
   publicIcon!: IconDefinition
   privateDecks!: Deck[]
   onlineDecks!: Deck[]
+  filteredPrivateDecks!: Deck[]
+  filteredOnlineDecks!: Deck[]
   showPrivateDecks!: boolean
+
+  @ViewChild('searchInput')
+  searchInput!: ElementRef;
 
   constructor(private injector: Injector, private router: Router) {
     this.deckRepository = this.injector.get<IDeckRepository>(DeckRepositoryService);
@@ -57,10 +62,30 @@ export class SearchComponent implements OnInit {
       return this.logout();
 
     this.privateDecks = await this.deckRepository.getPrivateDecks(user);
+    this.filteredPrivateDecks = this.privateDecks; //.map(deck => deck);
   }
 
   private async getOnlineDecks(): Promise<void> {
     this.onlineDecks = await this.deckRepository.getOnlineDecks();
+    this.filteredOnlineDecks = this.onlineDecks; //.map(deck => deck);
+  }
+
+  public onlineToggle(): void {
+    this.showPrivateDecks = !this.showPrivateDecks;
+    this.searchInput.nativeElement.value = '';
+    this.filteredPrivateDecks = this.privateDecks;
+    this.filteredOnlineDecks = this.onlineDecks;
+  }
+
+  public filterDecks(event: any): void {
+    if (this.showPrivateDecks)
+      this.filteredPrivateDecks = this.privateDecks.filter(deck =>
+        deck.name.toLowerCase().includes(event.target.value.toLowerCase())
+      );
+    else
+      this.filteredOnlineDecks = this.onlineDecks.filter(deck =>
+        deck.name.toLowerCase().includes(event.target.value.toLowerCase())
+      );
   }
 
   public logout(): void {
